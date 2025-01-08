9ee2ea1fc9b9c242e74b2eb6037a36a67521a6a2
Yan Kuang <YTKme@Outlook.com>
2025-01-08 00:12:59 -0800

    Revert Hono update

--- src/index.ts
+++ src/index.ts
@@ -2,38 +2,13 @@
  * CLOUDFLARE UniFi Dynamic Domain Name System (DDNS)
  */
 
-import {
-	Context,
-	Hono,
-} from "hono";
+export default {
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		// Debug
+		console.debug(`Request: ${JSON.stringify(request)}`);
+		console.debug(`Environment: ${JSON.stringify(env)}`);
+		console.debug(`Context: ${JSON.stringify(ctx)}`);
 
-const application = new Hono();
-
-application.get("/", async (context: Context) => {
-	console.debug(`Request: ${JSON.stringify(context.req)}`);
-	console.debug(`Environment: ${JSON.stringify(context.env)}`);
-
-	return context.body(
-		// Data
-		"Hello World!",
-		// Status
-		200,
-		// Header
-		{
-			"Content-Type": "text/plain",
-		}
-	)
-});
-
-// export default {
-// 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-// 		// Debug
-// 		console.debug(`Request: ${JSON.stringify(request)}`);
-// 		console.debug(`Environment: ${JSON.stringify(env)}`);
-// 		console.debug(`Context: ${JSON.stringify(ctx)}`);
-
-// 		return new Response('Hello World!');
-// 	},
-// } satisfies ExportedHandler<Env>;
-
-export default application;
+		return new Response('Hello World!');
+	},
+} satisfies ExportedHandler<Env>;
