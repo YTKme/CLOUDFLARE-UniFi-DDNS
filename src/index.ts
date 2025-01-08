8359f93d92d61e330f6ac2a7a2d43b4ae9bbeabc
Yan Kuang <YTKme@Outlook.com>
2025-01-08 00:11:51 -0800

    Update project to test Hono

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,38 @@
  * CLOUDFLARE UniFi Dynamic Domain Name System (DDNS)
  */
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+import {
+	Context,
+	Hono,
+} from "hono";
+
+const application = new Hono();
+
+application.get("/", async (context: Context) => {
+	console.debug(`Request: ${JSON.stringify(context.req)}`);
+	console.debug(`Environment: ${JSON.stringify(context.env)}`);
+
+	return context.body(
+		// Data
+		"Hello World!",
+		// Status
+		200,
+		// Header
+		{
+			"Content-Type": "text/plain",
+		}
+	)
+});
+
+// export default {
+// 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+// 		// Debug
+// 		console.debug(`Request: ${JSON.stringify(request)}`);
+// 		console.debug(`Environment: ${JSON.stringify(env)}`);
+// 		console.debug(`Context: ${JSON.stringify(ctx)}`);
+
+// 		return new Response('Hello World!');
+// 	},
+// } satisfies ExportedHandler<Env>;
+
+export default application;
