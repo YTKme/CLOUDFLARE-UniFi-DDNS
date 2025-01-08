fa0935797266b1dcc102f44f8c68862ccec51116
Yan Kuang <YTKme@Outlook.com>
2025-01-08 00:11:51 -0800

    Update project to test Hono

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,49 @@
  * CLOUDFLARE UniFi Dynamic Domain Name System (DDNS)
  */
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+import { Context, Hono } from "hono";
+import { ContentfulStatusCode } from "hono/utils/http-status";
+
+
+const application = new Hono();
+
+/**
+ * HTTP Error
+ * @extends Error
+ */
+class HttpError extends Error {
+  constructor(
+    public statusCode: ContentfulStatusCode,
+    message: string,
+  ) {
+    super(message);
+    this.name = 'HttpError';
+  }
+}
+
+// Default Route
+application.get("/", async (context: Context) => {
+  console.debug(`Request: ${JSON.stringify(context.req)}`);
+  console.debug(`Environment: ${JSON.stringify(context.env)}`);
+
+  try {
+
+  } catch (error) {
+    console.error(`Error: ${error}`);
+
+    // Handle HTTP Error
+    if (error instanceof HttpError) {
+      return context.body(
+        error.message,
+        error.statusCode as ContentfulStatusCode,
+      );
+    } else {
+      return context.body(
+        "Internal Server Error",
+        500,
+      );
+    }
+  }
+});
+
+export default application;
