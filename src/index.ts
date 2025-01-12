dffff316a412f449390409be813954f4a548959e
Yan Kuang <YTKme@Outlook.com>
2025-01-12 09:29:57 -0800

    Trying context.req.raw for client option

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
  * CLOUDFLARE UniFi Dynamic Domain Name System (DDNS)
  */
 
+import { ClientOptions } from "cloudflare";
 import { Context, Hono } from "hono";
 import { ContentfulStatusCode } from "hono/utils/http-status";
 
@@ -22,13 +23,40 @@ class HttpError extends Error {
   }
 }
 
+/**
+ * Get Client Option
+ */
+const getClientOption = (request: Request): ClientOptions => {
+  console.debug(`Request: ${JSON.stringify(request)}`);
+
+  const authorization = request.headers.get("Authorization");
+
+  if (!authorization) {
+    throw new HttpError(401, "Unauthorized: Invalid Token");
+  }
+
+  const [, data] = authorization.split(" ");
+  const decodedData = atob(data);
+  const index = decodedData.indexOf(":");
+
+  if (index === -1 || /[\0-\x1F\x7F]/.test(decodedData)) {
+    throw new HttpError(401, "Unauthorized: Invalid Token");
+  }
+
+  return {
+    apiEmail: decodedData.substring(0, index),
+    apiToken: decodedData.substring(index + 1),
+  };
+};
+
 // Default Route
 application.get("/", async (context: Context) => {
   console.debug(`Request: ${JSON.stringify(context.req)}`);
   console.debug(`Environment: ${JSON.stringify(context.env)}`);
 
   try {
-
+    // Get client option and DNS record
+    const clientOption = getClientOption(context.req.raw);
   } catch (error) {
     console.error(`Error: ${error}`);
 
